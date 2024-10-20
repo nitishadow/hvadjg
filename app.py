ac998a45824d687c4501ffbf8b7adf8c17b2e494
nitishadow <iamnitishsah12@gmail.com>
2024-10-20 23:44:07 +0530

    Commited

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-# app.py
-
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
 from model.lifestyle_model import predict_lifestyle_score
 
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})  # Allow localhost:63343
 
 # POST API to predict lifestyle score
 @app.route('/predict', methods=['POST'])
@@ -30,4 +30,3 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=4000)
-
